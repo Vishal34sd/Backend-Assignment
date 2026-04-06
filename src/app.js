f36483e614340a45c8d891f198322c3611f0470f
Vishal <dubeyvishal714@gmail.com>
2026-04-06 20:20:20 +0530

    completed assignment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 
 const app = express();
@@ -12,10 +10,8 @@ const limiter = rateLimit({
   standardHeaders: "draft-8"
 });
 
-app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
-app.use(morgan("dev"));
 app.use(limiter);
 
 app.get("/health", (req, res) => {
